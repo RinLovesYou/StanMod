0baa647c0f096e6979e8eb8b0934717171caec4a
Sarah <xxklintan123xx@gmail.com>
2022-05-16 19:01:03 +0200

    Add project files.

---
+++ StanMod/Class1.cs
@@ -0,0 +1,28 @@
+﻿using MelonLoader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace StanMod
+{
+    public class Class1 : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.R)) {
+                var objs = Resources.FindObjectsOfTypeAll<RotatingDoor>();
+                foreach (var obj in objs)
+                {
+                    if (obj is null)
+                        continue;
+                    obj.isLocked = false;
+                }
+            }
+        }
+    }
+
+}
